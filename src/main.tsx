ea0a52044202bd58060df0a53c42d0d105709ff8
Bondo_Dev <bondoasanidze95@gmail.com>
2026-03-07 18:54:26 +0400

    Remove manual GA tracking in favor of Zaraz

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { useEffect } from "react";
-import { useLocation } from "react-router";
 import App from "./app/App.tsx";
-import { trackPageView } from "./analytics";
 import "flag-icons/css/flag-icons.min.css";
 import "./styles/index.css";
 import "./styles/filters_page.scss";
@@ -13,19 +10,8 @@ import "./styles/about_help_page.scss";
 import "./styles/header.scss";
 import "./styles/exercises.scss";
 
-function AnalyticsTracker() {
-  const location = useLocation();
-
-  useEffect(() => {
-    trackPageView(`${location.pathname}${location.search}${location.hash}`);
-  }, [location.pathname, location.search, location.hash]);
-
-  return null;
-}
-
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <AnalyticsTracker />
     <App />
   </BrowserRouter>,
 );
